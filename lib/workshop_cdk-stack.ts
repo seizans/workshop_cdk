a75129cbc039cdddc313ffdd2e30ad7aa1238ef7
Seizan Shimazaki <seizan.shimazaki@aktsk.jp>
2024-02-06 23:03:55 +0900

    ec2 instance を追加する

--- lib/workshop_cdk-stack.ts
+++ lib/workshop_cdk-stack.ts
@@ -9,5 +9,13 @@ export class WorkshopCdkStack extends Stack {
     const vpc = new ec2.Vpc(this, "VlogVpc", {
       ipAddresses: ec2.IpAddresses.cidr('10.0.0.0/16')
     })
+
+    const webServer1 = new ec2.Instance(this, "WordpressServer1", {
+      vpc,
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.SMALL),
+      machineImage: new ec2.AmazonLinuxImage({
+        generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
+      })
+    });
   }
 }
