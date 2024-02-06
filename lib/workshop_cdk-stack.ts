c9c7c44bed8701a33abb781a8edd964902345d70
Seizan Shimazaki <seizan.shimazaki@aktsk.jp>
2024-02-06 18:42:09 +0900

    Vpc を作成する

--- lib/workshop_cdk-stack.ts
+++ lib/workshop_cdk-stack.ts
@@ -1,19 +1,13 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Stack, StackProps } from 'aws-cdk-lib';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import { Construct } from 'constructs';
 
 export class WorkshopCdkStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'WorkshopCdkQueue', {
-      visibilityTimeout: Duration.seconds(300)
-    });
-
-    const topic = new sns.Topic(this, 'WorkshopCdkTopic');
-
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    const vpc = new ec2.Vpc(this, "VlogVpc", {
+      ipAddresses: ec2.IpAddresses.cidr('10.0.0.0/16')
+    })
   }
 }
