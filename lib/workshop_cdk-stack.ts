58490b10faf1f7a4d39d132199ddeb4e9ba83cc3
Seizan Shimazaki <seizan.shimazaki@aktsk.jp>
2024-02-07 16:45:50 +0900

    RDS を作成する

--- lib/workshop_cdk-stack.ts
+++ lib/workshop_cdk-stack.ts
@@ -1,5 +1,6 @@
 import { CfnOutput, Stack, StackProps } from 'aws-cdk-lib';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as rds from 'aws-cdk-lib/aws-rds';
 import { Construct } from 'constructs';
 
 import { readFile, readFileSync } from 'fs';
@@ -25,6 +26,14 @@ export class WorkshopCdkStack extends Stack {
     webServer1.addUserData(script);
 
     webServer1.connections.allowFromAnyIpv4(ec2.Port.tcp(80));
+
+    const rdsServer = new rds.DatabaseInstance(this, "WordpressDB", {
+      vpc,
+      engine: rds.DatabaseInstanceEngine.mysql({version: rds.MysqlEngineVersion.VER_8_0_32}),
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.SMALL),
+      databaseName: "wordpress",
+    });
+    rdsServer.connections.allowDefaultPortFrom(webServer1);
 
     new CfnOutput(this, "WordpressServer1PublicIpAddress", {
       value: `http://${webServer1.instance.attrPublicIp}`,
